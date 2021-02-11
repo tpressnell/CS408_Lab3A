4177ca36304a7b0c363279becdbaa4278023906a
tyler <tyler.pressnell86@gmail.com>
2021-02-11 16:15:19 -0600

    Began refining the process() method.

--- app/src/main/java/com/example/lab3/Calculator.java
+++ app/src/main/java/com/example/lab3/Calculator.java
@@ -9,9 +9,11 @@ public class Calculator {
     private AppCompatActivity parent;
     private BigDecimal lValue;
     private BigDecimal rValue;
+    private BigDecimal answer;
     private StringBuilder inputBuffer;
     private StringBuilder displayBuffer;
     private boolean multipleOperands = false;
+    private boolean emptyValues = true;
 
     public Calculator(AppCompatActivity parent){
         this.parent = parent;
@@ -19,11 +21,50 @@ public class Calculator {
         displayBuffer = new StringBuilder();
         lValue = new BigDecimal(0);
         rValue = new BigDecimal(0);
+        answer = new BigDecimal(0);
 
     }
 
     public void process(String button){
-        if(button.equals(parent.getResources().getString(R.string.)))
+        if(button.equals(parent.getResources().getString(R.string.btnClear))){
+
+        }
+
+        else if(button.equals(parent.getResources().getString(R.string.btnSign))){
+
+        }
+
+        else if(button.equals(parent.getResources().getString(R.string.btnDecimal))){
+
+        }
+
+        else if(button.equals(parent.getResources().getString(R.string.btnSquareRoot))){
+
+        }
+
+        else if(button.equals(parent.getResources().getString(R.string.btnDivide))){
+
+        }
+
+        else if(button.equals(parent.getResources().getString(R.string.btnPercent))){
+
+        }
+
+        else if(button.equals(parent.getResources().getString(R.string.btnMultiply))){
+
+        }
+
+        else if(button.equals(parent.getResources().getString(R.string.btnMinus))){
+
+        }
+
+        else if(button.equals(parent.getResources().getString(R.string.btnPlus))){
+
+        }
+
+        else if(button.equals(parent.getResources().getString(R.string.btnEquals))){
+
+        }
     }
 
     public String getDisplay(){
