5866eca9b5671f34fd9d78ff6fe8a9f8d9424b45
tyler <tyler.pressnell86@gmail.com>
2021-02-18 15:14:35 -0600

    Completed logic for all operations

--- app/src/main/java/com/example/lab3/Calculator.java
+++ app/src/main/java/com/example/lab3/Calculator.java
@@ -25,161 +25,165 @@ public class Calculator {
 
 
     public void process(String button) {
-        if (button.equals(parent.getResources().getString(R.string.btnClear))) {
-            inputBuffer = new StringBuilder();
-            this.clearDisplay();
-        }
-        else if (button.equals(parent.getResources().getString(R.string.btnSign))) {
 
-        }
-        else if (button.equals(parent.getResources().getString(R.string.btnDecimal))) {
-            inputBuffer.append(button);
-            displayBuffer.append(button);
-        }
-        else if (button.equals(parent.getResources().getString(R.string.btnSquareRoot))) {
+        try {
+            if (button.equals(parent.getResources().getString(R.string.btnClear))) {
+                inputBuffer = new StringBuilder();
+                decimalUsed = false;
+                this.clearDisplay();
+            } else if (button.equals(parent.getResources().getString(R.string.btnSign))) {
+                lValue = new BigDecimal(inputBuffer.toString());
+                lValue = lValue.negate();
+                displayBuffer = new StringBuilder(lValue.toString());
+                inputBuffer = new StringBuilder();
 
-        }
-        else if (button.equals(parent.getResources().getString(R.string.btnDivide))) {
-            if (secondOperand == true) {
+            } else if (button.equals(parent.getResources().getString(R.string.btnDecimal))) {
+                if (decimalUsed == false) {
+                    inputBuffer.append(button);
+                    displayBuffer.append(button);
+                    decimalUsed = true;
+                }
+
+            } else if (button.equals(parent.getResources().getString(R.string.btnSquareRoot))) {
+                lValue = new BigDecimal(inputBuffer.toString());
+                double dValue = Double.parseDouble(lValue.toString());
+                double sqRoot = Math.pow(dValue, .5);
+                lValue = new BigDecimal(sqRoot);
+                displayBuffer = new StringBuilder(lValue.toString());
+                inputBuffer = new StringBuilder();
+            } else if (button.equals(parent.getResources().getString(R.string.btnDivide))) {
+                if (secondOperand == true) {
+                    rValue = new BigDecimal(inputBuffer.toString());
+                    answer = this.solve();
+                    inputBuffer = new StringBuilder();
+                    lValue = answer;
+                    rValue = new BigDecimal(0);
+                    this.clearDisplay();
+                    operator = button;
+                    decimalUsed = false;
+                } else {
+                    operator = button;
+                    this.clearDisplay();
+                    lValue = new BigDecimal(inputBuffer.toString());
+                    secondOperand = true;
+                    inputBuffer = new StringBuilder();
+                }
+            } else if (button.equals(parent.getResources().getString(R.string.btnPercent))) {
+                if (secondOperand == true) {
+                    rValue = new BigDecimal(inputBuffer.toString());
+                    answer = this.solve();
+                    inputBuffer = new StringBuilder();
+                    lValue = answer;
+                    rValue = new BigDecimal(0);
+                    this.clearDisplay();
+                    operator = button;
+                    decimalUsed = false;
+                } else {
+                    operator = button;
+                    this.clearDisplay();
+                    lValue = new BigDecimal(inputBuffer.toString());
+                    secondOperand = true;
+                    inputBuffer = new StringBuilder();
+                }
+            } else if (button.equals(parent.getResources().getString(R.string.btnMultiply))) {
+                if (secondOperand == true) {
+                    rValue = new BigDecimal(inputBuffer.toString());
+                    answer = this.solve();
+                    inputBuffer = new StringBuilder();
+                    lValue = answer;
+                    rValue = new BigDecimal(0);
+                    this.clearDisplay();
+                    operator = button;
+                    decimalUsed = false;
+                } else {
+                    operator = button;
+                    this.clearDisplay();
+                    lValue = new BigDecimal(inputBuffer.toString());
+                    secondOperand = true;
+                    inputBuffer = new StringBuilder();
+                }
+            } else if (button.equals(parent.getResources().getString(R.string.btnMinus))) {
+                if (secondOperand == true) {
+                    rValue = new BigDecimal(inputBuffer.toString());
+                    answer = this.solve();
+                    inputBuffer = new StringBuilder();
+                    lValue = answer;
+                    rValue = new BigDecimal(0);
+                    this.clearDisplay();
+                    operator = button;
+                    decimalUsed = false;
+
+                } else {
+                    operator = button;
+                    this.clearDisplay();
+                    lValue = new BigDecimal(inputBuffer.toString());
+                    secondOperand = true;
+                    inputBuffer = new StringBuilder();
+                }
+            } else if (button.equals(parent.getResources().getString(R.string.btnPlus))) {
+
+                if (secondOperand == true) {
+                    rValue = new BigDecimal(inputBuffer.toString());
+                    answer = this.solve();
+                    inputBuffer = new StringBuilder();
+                    lValue = answer;
+                    rValue = new BigDecimal(0);
+                    this.clearDisplay();
+                    operator = button;
+                    decimalUsed = false;
+                } else {
+                    operator = button;
+                    this.clearDisplay();
+                    lValue = new BigDecimal(inputBuffer.toString());
+                    secondOperand = true;
+                    inputBuffer = new StringBuilder();
+
+                }
+
+            } else if (button.equals(parent.getResources().getString(R.string.btnEquals))) {
                 rValue = new BigDecimal(inputBuffer.toString());
                 answer = this.solve();
+                displayBuffer = new StringBuilder(answer.toString());
                 inputBuffer = new StringBuilder();
-                lValue = answer;
+                lValue = new BigDecimal(0);
                 rValue = new BigDecimal(0);
-                this.clearDisplay();
-                operator = button;
-            }
-            else{
-                operator = button;
-                this.clearDisplay();
-                lValue = new BigDecimal(inputBuffer.toString());
-                secondOperand = true;
-                inputBuffer = new StringBuilder();
-            }
-        }
-        else if (button.equals(parent.getResources().getString(R.string.btnPercent))) {
-            if (secondOperand == true) {
-                rValue = new BigDecimal(inputBuffer.toString());
-                answer = this.solve();
-                inputBuffer = new StringBuilder();
-                lValue = answer;
-                rValue = new BigDecimal(0);
-                this.clearDisplay();
-                operator = button;
-            }
-            else{
-                operator = button;
-                this.clearDisplay();
-                lValue = new BigDecimal(inputBuffer.toString());
-                secondOperand = true;
-                inputBuffer = new StringBuilder();
-            }
-        }
-        else if (button.equals(parent.getResources().getString(R.string.btnMultiply))) {
-            if (secondOperand == true) {
-                rValue = new BigDecimal(inputBuffer.toString());
-                answer = this.solve();
-                inputBuffer = new StringBuilder();
-                lValue = answer;
-                rValue = new BigDecimal(0);
-                this.clearDisplay();
-                operator = button;
-            }
-            else{
-                operator = button;
-                this.clearDisplay();
-                lValue = new BigDecimal(inputBuffer.toString());
-                secondOperand = true;
-                inputBuffer = new StringBuilder();
-            }
-        }
-        else if (button.equals(parent.getResources().getString(R.string.btnMinus))) {
-            if (secondOperand == true) {
-                rValue = new BigDecimal(inputBuffer.toString());
-                answer = this.solve();
-                inputBuffer = new StringBuilder();
-                lValue = answer;
-                rValue = new BigDecimal(0);
-                this.clearDisplay();
-                operator = button;
-            }
-            else{
-                operator = button;
-                this.clearDisplay();
-                lValue = new BigDecimal(inputBuffer.toString());
-                secondOperand = true;
-                inputBuffer = new StringBuilder();
-            }
-        }
-        else if (button.equals(parent.getResources().getString(R.string.btnPlus))) {
+                secondOperand = false;
+                decimalUsed = false;
 
-            if (secondOperand == true) {
-                rValue = new BigDecimal(inputBuffer.toString());
-                answer = this.solve();
-                inputBuffer = new StringBuilder();
-                lValue = answer;
-                rValue = new BigDecimal(0);
-                this.clearDisplay();
-                operator = button;
+            } else if (button.equals(parent.getResources().getString(R.string._0))) {
+                inputBuffer.append(button);
+                displayBuffer.append(button);
+            } else if (button.equals(parent.getResources().getString(R.string._1))) {
+                inputBuffer.append(button);
+                displayBuffer.append(button);
+            } else if (button.equals(parent.getResources().getString(R.string._2))) {
+                inputBuffer.append(button);
+                displayBuffer.append(button);
+            } else if (button.equals(parent.getResources().getString(R.string._3))) {
+                inputBuffer.append(button);
+                displayBuffer.append(button);
+            } else if (button.equals(parent.getResources().getString(R.string._4))) {
+                inputBuffer.append(button);
+                displayBuffer.append(button);
+            } else if (button.equals(parent.getResources().getString(R.string._5))) {
+                inputBuffer.append(button);
+                displayBuffer.append(button);
+            } else if (button.equals(parent.getResources().getString(R.string._6))) {
+                inputBuffer.append(button);
+                displayBuffer.append(button);
+            } else if (button.equals(parent.getResources().getString(R.string._7))) {
+                inputBuffer.append(button);
+                displayBuffer.append(button);
+            } else if (button.equals(parent.getResources().getString(R.string._8))) {
+                inputBuffer.append(button);
+                displayBuffer.append(button);
+            } else if (button.equals(parent.getResources().getString(R.string._9))) {
+                inputBuffer.append(button);
+                displayBuffer.append(button);
             }
-            else{
-                operator = button;
-                this.clearDisplay();
-                lValue = new BigDecimal(inputBuffer.toString());
-                secondOperand = true;
-                inputBuffer = new StringBuilder();
-            }
-
         }
-        else if (button.equals(parent.getResources().getString(R.string.btnEquals))) {
-            rValue = new BigDecimal(inputBuffer.toString());
-            answer = this.solve();
-            displayBuffer = new StringBuilder(answer.toString());
-            inputBuffer = new StringBuilder();
-            lValue = new BigDecimal(0);
-            rValue = new BigDecimal(0);
-            secondOperand = false;
-
-        }
-        else if (button.equals(parent.getResources().getString(R.string._0))) {
-            inputBuffer.append(button);
-            displayBuffer.append(button);
-        }
-        else if (button.equals(parent.getResources().getString(R.string._1))) {
-            inputBuffer.append(button);
-            displayBuffer.append(button);
-        }
-        else if (button.equals(parent.getResources().getString(R.string._2))) {
-            inputBuffer.append(button);
-            displayBuffer.append(button);
-        }
-        else if (button.equals(parent.getResources().getString(R.string._3))) {
-            inputBuffer.append(button);
-            displayBuffer.append(button);
-        }
-        else if (button.equals(parent.getResources().getString(R.string._4))) {
-            inputBuffer.append(button);
-            displayBuffer.append(button);
-        }
-        else if (button.equals(parent.getResources().getString(R.string._5))) {
-            inputBuffer.append(button);
-            displayBuffer.append(button);
-        }
-        else if (button.equals(parent.getResources().getString(R.string._6))) {
-            inputBuffer.append(button);
-            displayBuffer.append(button);
-        }
-        else if (button.equals(parent.getResources().getString(R.string._7))) {
-            inputBuffer.append(button);
-            displayBuffer.append(button);
-        }
-        else if (button.equals(parent.getResources().getString(R.string._8))) {
-            inputBuffer.append(button);
-            displayBuffer.append(button);
-        }
-        else if (button.equals(parent.getResources().getString(R.string._9))) {
-            inputBuffer.append(button);
-            displayBuffer.append(button);
+        catch (Exception e){
+            displayBuffer = new StringBuilder("Press C to reset");
         }
     }
 
